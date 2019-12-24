2e8faa3ae18fcb20f8c6f1ef9cdf9ad824e0e6fd
unknown <bilelmek@gmail.com>
2019-12-24 16:52:18 +0100

    set up project socle (zuul proxy + eureka naming server + soap and rest micro services)

---
+++ magement-app/src/main/java/com/gestiondepartement/magementapp/MagementAppApplication.java
@@ -0,0 +1,16 @@
+package com.gestiondepartement.magementapp;
+
+import org.hibernate.validator.constraints.EAN;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagementAppApplication.class, args);
+	}
+
+}
---
+++ magement-app/src/test/java/com/gestiondepartement/magementapp/MagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestiondepartement.magementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
