56608ba2ad9fc3cc7b6b66e2f17e92730e8fb506
unknown <bilelmek@gmail.com>
2019-12-24 16:40:32 +0100

    origin master

---
+++ historique/src/main/java/com/gestiondepartement/historique/HistoriqueApplication.java
@@ -0,0 +1,15 @@
+package com.gestiondepartement.historique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class HistoriqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoriqueApplication.class, args);
+	}
+
+}
---
+++ historique/src/test/java/com/gestiondepartement/historique/HistoriqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestiondepartement.historique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoriqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
